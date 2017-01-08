6e931ead3643ea0d705a24ff962842de6307e6b9
Corey <coreycor904@outlook.com>
2017-01-08 03:34:03 +0000

    4.6 changes=sidenav/color-scheme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,14 @@
 //= require_tree .
 
 
+function openNav() {
+    document.getElementById("side-nav").style.width = "250px";
+    document.getElementById("main").style.marginLeft = "250px";
+    
+}
 
+function closeNav() {
+    document.getElementById("side-nav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    
+} 
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,7 +4,7 @@ class StaticPagesController < ApplicationController
 
 
   def landing_page 
- 	@products = Product.limit(3)
+ 	@products = Product.limit(4)
   end 
 end
  	
