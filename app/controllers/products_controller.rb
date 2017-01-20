a3f2cda40fbdc5969b375fc5f7211a72a05e6ce0
Corey <coreycor904@outlook.com>
2017-01-20 05:44:37 +0000

    5.2 form control added and ENV if conditional put in products controller

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -6,10 +6,15 @@ class ProductsController < ApplicationController
   def index
     if params[:q]
       search_term = params[:q]
-      @products  = Product.where("name LIKE ?", "%#{search_term}%")
+      if(Rails.env.development?)
+        @products = Product.where("name LIKE ?", "%#{search_term}%")
+      else
+        # use ilike for case insensitivity on postres
+        @products = Product.where("name ilike ?", "%#{search_term}%")
+      end
     else
     @products = Product.all
-  end
+    end
     
   end
     
