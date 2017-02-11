e9115c54f022371a29cc26f701464eb38ddf5693
Corey Cutlip <coreycor904@outlook.com>
2017-02-11 01:23:47 +0000

    fixed nesting in ability.rb and added can destroy comments

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -4,13 +4,13 @@ class Ability
   def initialize(user)
     # Define abilities for the passed in user here. For example:
     #
-  user ||= User.new # guest user (not logged in)
-  can :manage, User, id: user.id
-   if user.admin?
-    can :manage, :all
+    user ||= User.new # guest user (not logged in)
+    can :manage, User, id: user.id
+    if user.admin?
+    can :destroy, Comment
  
+    end
   end
- end
 end
   
     #   if user.admin?
