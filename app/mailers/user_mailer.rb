b4e8de88b87772fb7f157c42e165e2a4d5a8bbf0
Corey Cutlip <coreycor904@outlook.com>
2017-02-11 23:01:22 +0000

    5.11 added mailer to new clients

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -7,6 +7,11 @@ class UserMailer < ApplicationMailer
       :to => 'coreycor904@outlook.com', 
       :subject => "A new contact form message from #{name}")
   end
+  def welcome(user)
+  @appname = "Bike Shop"
+  mail( :to => user.email,
+        :subject => "Welcome to #{@appname}!")
+end
 end
 
 
