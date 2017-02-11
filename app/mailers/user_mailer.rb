0c05181825f3b8a47687a4eb9eb70c42e43e5393
Corey Cutlip <coreycor904@outlook.com>
2017-02-11 23:10:59 +0000

    5.11 welcome controller in mailer and a welcome email basic veiw

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,5 +1,5 @@
 class UserMailer < ApplicationMailer
-  default from: "from@example.com"
+  default from: "TeamBerlin"
 
   def contact_form(email, name, message)
     @message = message
@@ -8,7 +8,7 @@ class UserMailer < ApplicationMailer
       :subject => "A new contact form message from #{name}")
   end
   def welcome(user)
-  @appname = "Bike Shop"
+  @appname = "Bike Berlin"
   mail( :to => user.email,
         :subject => "Welcome to #{@appname}!")
 end
