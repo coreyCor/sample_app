2aaf9f789d5fdea68dac48ae41b2149d1485d781
Corey Cutlip <coreycor904@outlook.com>
2017-02-13 05:39:17 +0000

    add validations to product entry and made admins only able to acces new product link

--- app/models/product.rb
+++ app/models/product.rb
@@ -2,6 +2,12 @@ class Product < ApplicationRecord
 	has_many :orders
 	has_many :comments
 	
+	validates :name, presence: true
+	validates :colour, presence: true
+    validates :image_url, presence: true
+    validates :description, presence: true
+    validates :price, presence: true
+	
 	def highest_rating_comment
 		comments.rating_desc.first
 	end
