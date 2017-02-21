e2a34be28e68f9b334555004dfa8a4bd73d4cfb4
Corey Cutlip <coreycor904@outlook.com>
2017-02-21 05:26:25 +0000

    rspec unit test ran and minor layout changes to css on navbar

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,7 +2,7 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :product
   
-  validates :body, presence: true
+    validates :body, presence: true
     validates :user, presence: true
     validates :product, presence: true
     validates :rating, numericality: { only_interger: true}
