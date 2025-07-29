f2b194c7cf372714fb2b4a45242a0bba6e548a4c
Corey Cutlip <coreycor904@outlook.com>
2025-07-29 11:52:18 -0700

    Update product.rb

--- app/models/product.rb
+++ app/models/product.rb
@@ -4,9 +4,9 @@ class Product < ApplicationRecord
 	
 	validates :name, presence: true
 	validates :colour, presence: true
-    validates :image_url, presence: true
-    validates :description, presence: true
-    validates :price, presence: true
+   	validates :image_url, presence: true
+    	validates :description, presence: true
+    	validates :price, presence: true
 	
 	def highest_rating_comment
 		comments.rating_desc.first
